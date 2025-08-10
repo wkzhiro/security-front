71ef82ec606250d5dd40f815e3410336dd6ffe91
wkzhiro <domohiro0116@gmail.com>
2025-08-11 00:03:44 +0900

    微修正

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    domains: ['s.gravatar.com', 'cdn.auth0.com'],
+  },
 }
 
 export default nextConfig;
